978ebdbc795331a1890f304f58e8cc61da622c50
Francis Chung <francis.c.chung@gmail.com>
2026-02-19 22:47:11 +1100

    Record Count test

--- driver_intro.py
+++ driver_intro.py
@@ -1,9 +1,19 @@
 from neo4j import GraphDatabase
 driver = GraphDatabase.driver(
     "neo4j://localhost:7687",
-    auth=("neo4j", "neo4j")
+    auth=("neo4j", "neo4jd0m3")
 )
 driver.verify_connectivity()
 print("Driver connected")
+
+
+records, summary, keys = driver.execute_query( # (1)
+    "RETURN COUNT {()} AS count"
+)
+# Get the first record
+first = records[0]      # (2)
+# Print the count entry
+print(first["count"])   # (3)
+
 driver.close()
 print("Driver closed")
