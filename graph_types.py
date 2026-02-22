ab86850677524d622869f301e0e804977db4aa09
Francis Chung <francis.c.chung@gmail.com>
2026-02-23 00:16:17 +1100

    Relationships example

--- graph_types.py
+++ graph_types.py
@@ -11,6 +11,8 @@ driver = GraphDatabase.driver(
 
 movie = "Toy Story"
 
+## Graph
+
 records, summary, keys = driver.execute_query("""
 MATCH path = (person:Person)-[actedIn:ACTED_IN]->(movie:Movie {title: $title})
 RETURN path, person, actedIn, movie
@@ -20,10 +22,24 @@ RETURN path, person, actedIn, movie
 # print("summary", summary)
 # print("keys", keys)
 
+## Records
+
+# for record in records:
+#     node = record["movie"]
+#     print(node.element_id)  # (1)
+#     print(node.labels)  # (2)
+#     print(node.items())  # (3)
+#     print(node["name"])
+#     print(node.get("name", "N/A"))
+
+## Relationships
 for record in records:
-    node = record["movie"]
-    print(node.element_id)  # (1)
-    print(node.labels)  # (2)
-    print(node.items())  # (3)
-    print(node["name"])
-    print(node.get("name", "N/A"))
+    acted_in = record["actedIn"]
+    print(acted_in.id)         # (1)
+    print(acted_in.type)       # (2)
+    print(acted_in.items())    # (3)
+    # 4
+    print(acted_in["roles"])
+    print(acted_in.get("roles", "(Unknown)"))
+    print(acted_in.start_node) # (5)
+    print(acted_in.end_node)   # (6)
