1c6a6b3b495730f70a234c824cc5d0004ee5393c
Francis Chung <francis.c.chung@gmail.com>
2026-02-22 19:49:34 +1100

    result_transformer example

--- executing_cypher.py
+++ executing_cypher.py
@@ -10,15 +10,17 @@ cypher = """
 MATCH (p:Person {name: $name})-[r:ACTED_IN]->(m:Movie)
 RETURN m.title AS title, r.role AS role
 """
-
 name = "Tom Hanks"
 
-records, summary, keys = driver.execute_query(
+result = driver.execute_query(
     cypher,
-    name=name
+    name=name,
+    result_transformer_= lambda result: [
+        f"Tom Hanks played {record['role']} in {record['title']}"
+        for record in result
+    ]
 )
-
-print(records)
+print(result)
 
 
 driver.close()
